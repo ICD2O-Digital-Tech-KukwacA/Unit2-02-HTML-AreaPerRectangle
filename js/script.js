c9f4772808076ac932ace8608fd12f1aac44717e
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-03-21 14:47:05 +0000

    My second web page with JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Ms Raffin All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kukwac
+// Created on: Mar 2022
 // This file contains the JS functions for index.html
+
+function doMathClicked() {
+  // this function does basic math
+  document.getElementById('multiply-math').innerHTML = 'The area of a 10" by 44" rectangle = ' + (10 * 44) + '"^2';
+  document.getElementById('add-math').innerHTML = 'The perimeter of the rectangle = ' + (10 + 44) + '"';
+}
