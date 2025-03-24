8bf753c1d0b0b1beec8d58149ff0bb0cc587adee
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-03-24 14:58:14 +0000

    Area and Perimeter of a Rectangle in PHP

--- index.php
+++ index.php
@@ -7,10 +7,10 @@
     <meta name="keywords" content="immaculata, ics2o">
     <meta name="author" content="Kukwac">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="apple-touch-icon" sizes="180x180" href="./Favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="./Favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="./Favicon/favicon-16x16.png">
-    <link rel="manifest" href="./Favicon/site.webmanifest">
+    <link rel="apple-touch-icon" sizes="180x180" href="./Favicons_2/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./Favicons_2/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./Favicons_2/favicon-16x16.png">
+    <link rel="manifest" href="./Favicons_2/site.webmanifest">
     <title>Area & Perimeter of Rectangle in PHP</title>
 	</head>
 	
@@ -18,7 +18,7 @@
     <h1><?php echo 'Area & Perimeter of a Rectangle'; ?></h1>
 		<?php 
 			$length = 14;  
-		 	$width = 12; 
+			$width = 12; 
 			echo "<p>Length = $length cm<br>Width = $width cm</p>"; ?>		
 		<?php 
 		 echo "<p>The area of a rectangle with dimensions $length cm x $width cm = " . ($length * $width) . "cm<sup>2</sup></p>";?>
